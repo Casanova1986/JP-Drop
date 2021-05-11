e67cf836aa0809364390bcdb9244ee6fc4dba1ec
Casanova1986 <tudt.erp@gmail.com>
2021-05-12 00:17:29 +0700

    add code

---
+++ Assets/Script/Option2QuestManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Option2QuestManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+
+    public void OnTouchDown(BaseEventData eventData)
+    {
+
+        Debug.Log("OnTouchDown");
+      
+    }
+
+    public void OnMove()
+    {
+        Debug.Log("OnMove");
+    }
+
+    public void OnRelease()
+    {
+        Debug.Log("OnRelease");
+    }
+}
